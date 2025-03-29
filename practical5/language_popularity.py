4248f376a7043dc1d21e8ffbe6b9eacda163b2f8
Liu Yinuo <yinuo.24@intl.zju.edu.cn>
2025-03-29 16:26:43 +0800

    language_population

--- practical5/language_popularity.py
+++ practical5/language_popularity.py
@@ -1,5 +1,6 @@
 #store the data into dictionary
 language = {'JavaScript':62.3,'HTML':52.9,'Python': 51, 'SQL':51, 'TypeScript': 38.5}
+print("(1) Dictionary:", language) #output the dictionary
 
 #draw the bar graph
 import matplotlib.pyplot as plt
@@ -15,8 +16,3 @@ if input_language in language:
     print(f"The Percentage of developers using {input_language}: {language[input_language]}%")
 else:
     print(f"{input_language} not found in the data. Available languages are: {list(language.keys())}")
-
-# Return all required components
-print("(1) Dictionary:", language)
-print(f"(2) Percentage for {input_language}: {language.get(input_language, 'N/A')}%")
-
