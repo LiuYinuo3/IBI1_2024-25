bb0b9f2ed5c3d6f25953adf150764a73352497b1
Liu Yinuo <yinuo.24@intl.zju.edu.cn>
2025-04-25 10:56:39 +0800

    rolling two dices to get the same numbers

---
+++ practical4/mystery_code.py
@@ -0,0 +1,20 @@
+# What does this piece of code do?
+# Answer: rolling the two dices ramdomly, how many rolls does it take until the two dice is the same
+
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+
+progress=0 #the times of rolling the dice, regulate the progress number to 0 in the beginning
+while progress>=0: # the time of the progress is not smaller than 0
+	progress+=1 # when  rolling for another time, plus the progress time
+	first_n = randint(1,6) #  the number of the first dice
+	second_n = randint(1,6) # the number of the second dice
+	if first_n == second_n: # check if they are the same
+		print(progress) # print the roll count
+		break #ending the program
